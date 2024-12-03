bc8fadee833743a48a1af5214d338adcaeace29c
Torleiv Håland Bryne <torleiv.h.bryne@ntnu.no>
2024-12-03 18:47:07 +0100

    Post processing/rawIFmetadata:  Improved printouts further

--- post_processing_script/process_rawIF_metadata.m
+++ post_processing_script/process_rawIF_metadata.m
@@ -2,6 +2,7 @@
 % process_rawIF_metadata.m
 %
 % STG, Sept 2017, GPLv3
+% Modified: THB, Des. 2024
 %
 satid_hex2FM = [247 249 43 44 47 54 55 73];
 %0xF7 (247): CYGNSS 1
@@ -17,33 +18,46 @@ satid_hex2FM = [247 249 43 44 47 54 55 73];
 %fid = fopen('../../../CYGNSS/RawIF/May15_Australia/RawIF_Data/cyg08_raw_if_20180515_215055_meta.bin');
 %fid = fopen('/media/gleason/Elements/CYGNSS_Data/rawIF/136/cyg08_raw_if_20200515_231925_meta.bin');
 
-display_time            = true;
-% display_time            = false;
-display_front_end_info  = true;
-% display_front_end_info  = false;
-data_path               = '/Users/thb/Kode/NTNU/gnssr-processing/CYGNSS-Processing/makefile_build_c/RawIFData'; % can be any location on your computer
+display_lines_seperating_meta_data  = true;
+% display_lines_seperating_meta_data  = false;
+display_file_name                   = true;
+display_file_name                   = false;
+display_sat_id                      = true;
+% display_sat_id                      = false;
+display_time                        = true;
+% display_time                        = false;
+display_front_end_info              = true;
+% display_front_end_info              = false;
+display_dataformat_info             = true;
+% display_dataformat_info             = false;
+%data_path               = '/Users/thb/Kode/NTNU/CYGNSS-Processing/makefile_build_c/RawIFData'; % can be any location on your computer
+data_path               = '/Volumes/gnss-r/CYGNSS DATA/'; % can be any location on your computer
 meta_files              = dir( fullfile( data_path, '*meta.bin') );
 n_files                 = length( meta_files );
 fprintf('Number of meta files found in path: %d\n', n_files);
+c_data_format = [0 0 0 0 0]; % counter
 for idx = 1:n_files
 % raw_data_meta_file      = 'cyg05_raw_if_s20220727_035832_e20220727_035933_meta.bin'; % can be any cygnss *_meta.bin file
-
-    fprintf('____________________________________________________________________________\n')
+    if display_lines_seperating_meta_data
+        fprintf('____________________________________________________________________________\n')
+    end
     file_name = meta_files(idx).name;
-    fprintf('File name: %s\n', file_name);
+    if display_file_name
+        fprintf('File name: %s\n', file_name);
+    end
 
     fid                     = fopen( fullfile( data_path, meta_files(idx).name) );
-    
+
     satIDhex                = dec2hex(fread(fid,1,'uint8')); % hex ID of satellite (need "decoder ring" to match to FM#)
     satIDdec                = hex2dec(satIDhex);
     satID_FM                = find(satid_hex2FM == satIDdec);
-    
+
     hdr                     = num2str(fread(fid,4,'char'),'%s'); % "DRT0" header
     gpsweek                 = fread(fid,1,'uint16','ieee-be'); % GPS week (read in Big Endian order)
     gpssecs                 = fread(fid,1,'uint32','ieee-be'); % GPS seconds (as 4 byte uint)
     dataformat              = fread(fid,1,'uint8'); % decimal
     samplingrate            = fread(fid,1,'uint32','ieee-be'); % sampling rate % (read in Big Endian order)
-    
+
     ch0FrontendSelection    = fread(fid,1,'uint8'); % (read in Big Endian order)
     ch0LOFrequency          = fread(fid,1,'uint32','ieee-be'); % frequnecy (Hz) % (read in Big Endian order)
     ch1FrontendSelection    = fread(fid,1,'uint8'); % (read in Big Endian order)
@@ -52,19 +66,20 @@ for idx = 1:n_files
     ch2LOFrequency          = fread(fid,1,'uint32','ieee-be'); % frequnecy (Hz) % (read in Big Endian order)
     ch3FrontendSelection    = fread(fid,1,'uint8'); % (read in Big Endian order)
     ch3LOFrequency          = fread(fid,1,'uint32','ieee-be'); % frequnecy (Hz) % (read in Big Endian order)
-    
+
     % See 148-0354-2 CYGNSS Raw IF Data File Format.pdf for rest of data entries
     % The gpssecs and dataformat are the 2 most important
-    
-    %satIDhex
+
+    % satIDhex
     % satID_FM
     % gpsweek
     % gpssecs
-    
-    fprintf('Sat id: %d(0x%s). Sampling rate: %.3f MHz\n', satID_FM, satIDhex, double(samplingrate)*1e-6)
+    if display_sat_id 
+        fprintf('Sat id: %d(0x%s). Sampling rate: %.3f MHz\n', satID_FM, satIDhex, double(samplingrate)*1e-6)
+    end
     if display_time
         fprintf('GPS week: %d. GPS sec: %d\n', gpsweek, gpssecs );
-        
+
         year_start          = str2double( file_name(15:18) );
         month_start         = str2double( file_name(19:20) );
         day_start           = str2double( file_name(21:22) );
@@ -90,10 +105,10 @@ for idx = 1:n_files
         % calculate UTC date from GPS time
         if exist('OCTAVE_VERSION', 'builtin') > 0 % running octave
             % datatime not yet implemented in Octave
-        else            
+        else
             sec_per_week                    = 604800;    % 3600*24*7
             diff_gps_time_utc_time          = 18;         % number of leap seconds
-            numb_weeks                      = gpsweek;   % should NOT add another week due to zero index since numb_weeks are the weeks that are already passed 
+            numb_weeks                      = gpsweek;   % should NOT add another week due to zero index since numb_weeks are the weeks that are already passed
             sec_from_gps_start              = numb_weeks*sec_per_week + gpssecs - diff_gps_time_utc_time;
 
             ReferenceDate = datetime('06/01/1980',...
@@ -102,7 +117,7 @@ for idx = 1:n_files
             numb_days   = sec_from_gps_start/(3600*24);
             Days        = caldays( floor( numb_days ) );
             numb_hours  = (numb_days - floor(numb_days))*24;
-            Hours       = hours( floor(numb_hours) ); 
+            Hours       = hours( floor(numb_hours) );
             numb_min    = (numb_hours - floor(numb_hours))*60;
             Minutes     = minutes( floor(numb_min) );
             numb_sec    = (numb_min - floor(numb_min))*60;
@@ -118,21 +133,43 @@ for idx = 1:n_files
         fprintf('Channel 2: Front end selection = %d. LO Frequency %.3f MHz\n', ch2FrontendSelection, double(ch2LOFrequency)*1e-6)
         fprintf('Channel 3: Front end selection = %d. LO Frequency %.3f MHz\n', ch3FrontendSelection, double(ch3LOFrequency)*1e-6)
     end
-    
     if(dataformat == 0)
-      disp('Channel 1, I Only ... double check, unusual')
-    elseif(dataformat == 1)   
-      disp('Channel 1 and 2, I Only ... double check, unusual')
-    elseif(dataformat == 2)   
-      disp('Channel 1,2,3, I Only  ... default for most collections')
-    elseif(dataformat == 3)   
-      disp('Channel 1,2,3,4 I Only  ... double check, there is no channel 4 connected')
-    elseif(dataformat == 4)   
-      disp('Channel 1,2 I and Q ... double check  unusual, will not work with processor')
+        if display_dataformat_info
+            disp('Channel 1, I Only ... double check, unusual')
+        end
+        c_data_format(dataformat+1) = c_data_format(dataformat+1) + 1;
+    elseif(dataformat == 1)
+        if display_dataformat_info
+            disp('Channel 1 and 2, I Only ... double check, unusual')
+        end
+        c_data_format(dataformat+1) = c_data_format(dataformat+1) + 1;
+    elseif(dataformat == 2)
+        if display_dataformat_info
+            disp('Channel 1,2,3, I Only  ... default for most collections')
+        end
+        c_data_format(dataformat+1) = c_data_format(dataformat+1) + 1;
+    elseif(dataformat == 3)
+        if display_dataformat_info
+            disp('Channel 1,2,3,4 I Only  ... double check, there is no channel 4 connected')
+        end
+        c_data_format(dataformat+1) = c_data_format(dataformat+1) + 1;
+    elseif(dataformat == 4)
+        if display_dataformat_info
+            disp('Channel 1,2 I and Q ... double check  unusual, will not work with processor')
+        end
+        c_data_format(dataformat+1) = c_data_format(dataformat+1) + 1;
     else
-      disp('Invalid format type')
-    end  
-    fprintf('____________________________________________________________________________\n')
-    
+        disp('Invalid format type')
+    end
     fclose(fid);
 end
+if display_lines_seperating_meta_data
+    fprintf('____________________________________________________________________________\n')
+end
+
+fprintf('\nTotal meta files found in path: %d\n', n_files);
+fprintf('Number of ''Channel 1, I Only (unusual)'' files: %d\n', c_data_format(1))
+fprintf('Number of ''Channel 1 and 2, I Only (unusual)'' files: %d\n', c_data_format(2))
+fprintf('Number of ''Channel 1,2,3, I Only (default)'' files: %d\n', c_data_format(3))
+fprintf('Number of ''Channel 1,2,3,4 I Only (check, there is no channel 4 connected)'' files: %d\n', c_data_format(4))
+fprintf('Number of ''Channel 1,2 I and Q (unusual, will not work with processor)'' files: %d\n', c_data_format(5))
