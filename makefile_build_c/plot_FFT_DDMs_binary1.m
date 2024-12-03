6a9deac6541d5ba7166082d01885213351145025
Torleiv Håland Bryne <torleiv.h.bryne@ntnu.no>
2024-12-03 19:18:25 +0100

    build-c/plot_FFR_DDM_s_binary1.m Improved title. SC read from meta data

--- makefile_build_c/plot_FFT_DDMs_binary1.m
+++ makefile_build_c/plot_FFT_DDMs_binary1.m
@@ -2,28 +2,63 @@
 %
 % plot_FFT_DDMs_binary1.m
 %
+% Modified by THB, 2024
 
 close all
 clear all
+
+%%
 colors = ['k' 'b' 'g' 'r' 'm' 'c'];
+satid_hex2FM = [247 249 43 44 47 54 55 73];
+%0xF7 (247): CYGNSS 1
+%0xF9 (249): CYGNSS 2
+%0x2B ( 43): CYGNSS 3
+%0x2C ( 44): CYGNSS 4
+%0x2F ( 47): CYGNSS 5
+%0x36 ( 54): CYGNSS 6
+%0x37 ( 55): CYGNSS 7
+%0x49 ( 73): CYGNSS 8
 
-sc_id = 8;
+%% Path Config
+% path to processed DDM. Default 'Processed_DDMs.bin'
+ddm_bin_file_name = 'Processed_DDMs_cyg04_raw_if_s20171130_234500_e20171130_234600_data';
+%ddm_bin_file_name = 'Processed_DDMs_cyg01_raw_if_s20180822_142659_e20180822_142759_data';
+%ddm_bin_file_name = 'Processed_DDMs_cyg08_raw_if_s20171230_000830_e20171230_000930_data';
 
-%plot_full_ddms = 0;
-plot_full_ddms = 1;
 
-%plot_cropped_ddms = 0;
-plot_cropped_ddms = 1;
+meta_data_path          = '/Users/thb/Kode/NTNU/CYGNSS-Processing/makefile_build_c/RawIFData'; % path of the processed raw if data's meta
+meta_data_file_name     = 'cyg04_raw_if_s20171130_234500_e20171130_234600_meta.bin';
+%meta_data_file_name     = 'cyg01_raw_if_s20180822_142659_e20180822_142759_meta.bin';
+%meta_data_file_name     = 'cyg08_raw_if_s20171230_000830_e20171230_000930_meta.bin';
+meta_data_file          = fullfile( meta_data_path, meta_data_file_name );
 
-%plot_delay_waveforms = 0;
-plot_delay_waveforms = 1;
+% finding space craft ID
+fid                     = fopen( meta_data_file );
+satIDhex                = dec2hex(fread(fid,1,'uint8')); % hex ID of satellite (need "decoder ring" to match to FM#)
+satIDdec                = hex2dec(satIDhex);
+satID_FM                = find(satid_hex2FM == satIDdec);
+sc_id                   = satID_FM;
 
-binaryFilename = sprintf('Processed_DDMs.bin');
+%% Config plotting
+%plot_full_ddms          = 0;
+plot_full_ddms          = 1;
+
+%plot_cropped_ddms       = 0;
+plot_cropped_ddms       = 1;
+
+%plot_delay_waveforms    = 0;
+plot_delay_waveforms    = 1;
+
+%store_images            = 0;
+store_images            = 1;
+
+%% Generating images
+binaryFilename = sprintf('%s.bin',ddm_bin_file_name);
 
 % set cropped DDM size
 buffer_delay_size = 200;  % samples
-buffer_dopp_size = 20; 
-%buffer_dopp_size = 48; 
+buffer_dopp_size = 20;
+%buffer_dopp_size = 48;
 
 fid = fopen(binaryFilename);
 
@@ -34,6 +69,7 @@ if(plot_cropped_ddms == 1)
   % cropped DDM
   figure(100);
   colorbar;
+  pause(1)
 end
 
 if(plot_full_ddms == 1)
@@ -87,23 +123,31 @@ for DDM_idx = 1:numDDMentries
   SNR_dB(DDM_idx) = 10*log10(signal_max(DDM_idx)/noise_temp);
   Max_Doppler(DDM_idx) = Doppler_axis(max_dopp_idx);
   Max_Delay(DDM_idx) = Delay_axis(max_delay_idx);
-  
+
   if(SNR_dB(DDM_idx) > 3)
-    PRN(DDM_idx) 
+    PRN(DDM_idx)
     Max_Doppler(DDM_idx)
     SNR_dB(DDM_idx)
   end
-  
+
 if(plot_delay_waveforms == 1)
-  figure(1);
-  color_idx = mod(DDM_idx,length(colors));
-  if(color_idx == 0)
-    color_idx = 6;
-  end
- 	plot(Delay_axis,DDM(max_dopp_idx,:),Color=colors(color_idx));
-% 	plot(DDM(max_dopp_idx,:),Color=colors(color_idx));
+    figure(1);
+    color_idx = mod(DDM_idx,length(colors));
+    if(color_idx == 0)
+        color_idx = 6;
+    end
+    plot(Delay_axis,DDM(max_dopp_idx,:),Color=colors(color_idx));
+    % 	plot(DDM(max_dopp_idx,:),Color=colors(color_idx));
+
+    str = sprintf("Delay Waveform: SC %d, PRN %d, DDM Number %d",sc_id, PRN(DDM_idx),DDM_idx);
+    title(str)
+    if store_images
+        pause(1)
+        print ("-r600", sprintf("%s_wave_form_ddm.png",ddm_bin_file_name));
+    end
+
 end
-  
+
 if(plot_cropped_ddms == 1)
   % plot cropped DDM
   figure(100);
@@ -113,14 +157,17 @@ if(plot_cropped_ddms == 1)
   xlabel('Doppler Bins');
 %  ylabel('Delay (chips)');
 %  xlabel('Doppler (Hz)');
-  str = sprintf("Raw IF Processed DDM, S/C %d, PRN %d, DDM Number %d",sc_id, PRN(DDM_idx),DDM_idx);
+  str = sprintf("Raw IF Processed DDM: SC %d, PRN %d, DDM Number %d",sc_id, PRN(DDM_idx),DDM_idx);
   title(str)
   drawnow;
 %  axis([3000 8000 470 490])
+  pause(1)
+  print ("-r600", sprintf("%s_cropped_ddm.png",ddm_bin_file_name));
+
 
 if(0)
-      filename = ['images/' num2str(DDM_idx,"%03d") '_imageframe.jpg'];
-      print(filename,'-djpg')
+    filename = ['images/' num2str(DDM_idx,"%03d") '_imageframe.jpg'];
+    print(filename,'-djpg')
 end
 
 
@@ -133,18 +180,21 @@ if(plot_full_ddms == 1)
   imagesc(DDM);
   ylabel('Delay (Code Phase)');
   xlabel('Doppler (Hz)');
-  str = sprintf("Full DDM for PRN %d, DDM Number %d",PRN(DDM_idx),DDM_idx);
+  str = sprintf("Full DDM: SC %d PRN %d, DDM Number %d",sc_id, PRN(DDM_idx),DDM_idx);
   title(str)
-  colorbar;
+  title(str)
+  colorbar
+  pause(1)
+  print ("-r600", sprintf("%s_full_ddm.png",ddm_bin_file_name));
 %  axis([startDoppler(DDM_idx) endDoppler(DDM_idx) 1 numDelays(DDM_idx) 0 6e12])
-end  
-  
+end
+
   pause(1);
- 
+
 end  % end DDM loop
 
 %PRN
 %Max_Doppler
 %Max_Delay
-  
-  
+
+
